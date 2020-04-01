b2c9a0b52689442922217f8f2d8abaef8be719da
Victor Kayan <victor.vkal@gmail.com>
2020-04-01 11:58:26 -0300

    Complete Cytogenetics glossary data

--- assets/glossaryData/CytogeneticsData/index.js
+++ assets/glossaryData/CytogeneticsData/index.js
@@ -3,24 +3,11 @@ const cytogeneticsData = [
     sectionTitle: 'A',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Aneuploidia',
+        definition: 'É a segunda maior categoria de mutações cromossômicas que envolvem alteração no número de cromossomos. As aneuploidias são causadas em sua maioria por uma não-disjunção durante a meiose ou a mitose. A não-disjunção é um erro no processo de segregação, onde dois cromossomos ou cromátides vão para um polo e nenhum vai para o polo oposto na divisão meiótica ou mitótica.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  }, 
-  {
-    sectionTitle: 'B',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://www.infoescola.com/genetica/aneuploidia/' },
+          { url: 'https://spmr.pt/index.php/16-repropedia/a/64-aneuploidia' }
         ]
       }
     ]
@@ -29,11 +16,31 @@ const cytogeneticsData = [
     sectionTitle: 'C',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Cariótipo',
+        definition: 'É o nome dado ao conjunto de cromossomos de uma dada espécie e apresenta forma, tamanho e número característicos. Ao preparar o cariótipo, os cromossomos condensados são arranjados em pares. Essa organização permite avaliar todas essas estruturas de uma determinada espécie.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://www.biologianet.com/genetica/cariotipo.htm' },
+        ]
+      },
+      {
+        concept: 'Centrômero',
+        definition: 'É a região onde um cromossomo presente em uma célula que está participando de um processo de divisão celular apresenta maior condensação.',
+        trustableSites: [
+          { url: 'https://repositorio.ufsm.br/handle/1/5384' },
+        ]
+      },
+      {
+        concept: 'Cromossomo em anel',
+        definition: 'É uma alteração estrutural que surge quando acontece uma quebra nas duas extremidades do cromossomo (braço curto e braço longo) e uma subsequente fusão. Poderá haver consequências fenotípicas para o indivíduo que for portador dessa anomalia, tanto pela deleção da cromatina das duas pontas, como também pela instabilidade mitótica dos anéis, resultando em células trissômicas ou monossomias.',
+        trustableSites: [
+          { url: 'https://core.ac.uk/download/pdf/37712849.pdf' },
+        ]
+      },
+      {
+        concept: 'Cromossomo',
+        definition: 'São estruturas formadas por uma molécula de DNA associada a moléculas proteicas, um complexo chamado de cromatina. As proteínas associadas ajudam a enrolar a molécula de DNA, reduzindo seu comprimento. Na cromatina, observamos a presença, principalmente, das proteínas denominadas de histonas.',
+        trustableSites: [
+          { url: 'https://www.biologianet.com/biologia-celular/cromossomos.htm' },
         ]
       }
     ]
@@ -42,11 +49,34 @@ const cytogeneticsData = [
     sectionTitle: 'D',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Deleção',
+        definition: 'Corresponde a perda de parte de um cromossomo, em geral envolvendo um ou mais genes (raramente uma parte de um gene). É a perda de sequências nucleotídicas pelo genoma de um organismo.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'http://www.biologia.seed.pr.gov.br/modules/galeria/detalhe.php?foto=60&evento=2' },
+        ]
+      },
+      {
+        concept: 'DNA Microssatélites',
+        definition: 'São unidades de repetição de pares de bases do DNA (AT, GC), que são utilizados como marcador genético em estudos de parentesco, as migrações humanas e de origem humana. Os microssatélites apresentam taxas mais altas de mutação genética em relação a outras partes do DNA e, portanto, perfeito para estudar ancestralidade humana. Essas unidades possuem de 1 a 4 nucleotídeos de comprimento e sequência repetitiva constituída de 2 a 9 pares de bases.',
+        trustableSites: [
+          { url: 'http://www.scielo.br/scielo.php?script=sci_arttext&pid=S0102-09352007000500024' },
+          { url: 'https://siteantigo.portaleducacao.com.br/conteudo/artigos/medicina/marcadores-de-variacao-genetica/24559' }
+        ]
+      },
+      {
+        concept: 'DNA Minissatélites',
+        definition: 'Os minissatélites têm sido implicados como reguladores da expressão de genes. Eles são, geralmente, não-codificante de DNA, mas, por vezes, são parte de possíveis genes. Além dissso, constituem os telómeros cromossómicas. Os minissatélites são sequências de 6 a 100 nucleotídeos repetidos e enfileirados de DNA e, também, estruturas que possuem regiões repetitivas constituídas de 10 a 64 pares de bases.',
+        trustableSites: [
+          { url: 'https://www.alice.cnptia.embrapa.br/handle/doc/873069' },
+          { url: 'https://siteantigo.portaleducacao.com.br/conteudo/artigos/medicina/marcadores-de-variacao-genetica/24559' }
+        ]
+      },
+      {
+        concept: 'Duplicação',
+        definition: 'A duplicação implica em qualquer duplicação de uma região de DNA que contém um gene; pode ocorrer como um erro na recombinação homóloga, um evento de retro transposição ou duplicação de um cromossoma inteiro.',
+        trustableSites: [
+          { url: 'https://www.sobiologia.com.br/conteudos/Citologia2/AcNucleico3.php' },
+          { url: 'https://youtu.be/BkQXUSmi0Wk' }
         ]
       }
     ]
@@ -55,24 +85,17 @@ const cytogeneticsData = [
     sectionTitle: 'E',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Epigenética',
+        definition: 'É a área da biologia que estuda mudanças no funcionamento de um gene que não são causadas por alterações na sequência de DNA e que se perpetuam nas divisões celulares, meióticas ou mitóticas. É denominado um caráter epigenético quando o fenótipo é causado por tais mudanças.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'http://www.ecetoc.org/pt/topicos/epigenetica/' },
         ]
-      }
-    ]
-  }, 
-  {
-    sectionTitle: 'F',
-    data: [
+      },
       {
-        concept: '',
-        definition: '',
+        concept: 'Eucromatina',
+        definition: 'É o estado descondensado da cromatina. Nessas áreas, os nucleossomas afastam-se uns dos outros, expondo os genes para que possam, assim, realizar a transcrição.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://www.lifeder.com/eucromatina/' },
         ]
       }
     ]
@@ -81,11 +104,21 @@ const cytogeneticsData = [
     sectionTitle: 'G',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Gêmeos bivitelinos',
+        definition: 'Acontece em decorrência da fecundação de dois óvulos por dois espermatozoides que carregam material genético diferente um do outro, assim como o de um óvulo é diferente do outro.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://www.sobiologia.com.br/conteudos/embriologia/reproducao16.php' },
+          { url: 'http://www.blog.saude.gov.br/index.php/geral/53895-saiba-a-diferenca-entre-gemeos-univitelinos-e-bivitelinos' },
+          { url: 'https://drauziovarella.uol.com.br/entrevistas-2/gemeos-entrevista/amp/' }
+        ]
+      },
+      {
+        concept: 'Gêmeos univitelinos',
+        definition: 'É o resultado da fecundação de um óvulo por um espermatozoide, que posteriormente acaba sofrendo uma divisão. Como a divisão é de um óvulo, o material genético é idêntico.',
+        trustableSites: [
+          { url: 'https://www.sobiologia.com.br/conteudos/embriologia/reproducao16.php' },
+          { url: 'https://drauziovarella.uol.com.br/entrevistas-2/gemeos-entrevista/amp/' },
+          { url: 'https://periodicos.unifesp.br/index.php/neurociencias/article/view/8609' }
         ]
       }
     ]
@@ -94,102 +127,47 @@ const cytogeneticsData = [
     sectionTitle: 'H',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Heterocromatina',
+        definition: 'É o estado condensado da cromatina, ou seja, quando os genes estão inativados, não podendo participar do processo de transcrição.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'http://www.ufrgs.br/biologiacelularatlas/nucleo.htm' },
+          { url: 'https://blogdoenem.com.br/biologia-khan-academy-2/' }
+        ]
+      },
+      {
+        concept: 'Histonas',
+        definition: 'São as principais proteínas presentes no nucleossomo. Possui um importante papel na regulação dos genes, sendo encontradas no núcleo de células eucarióticas. Existem seis tipos de histonas conhecidas, são elas: H1, H2A, H2B, H3, H4 e Histonas de Archaea.',
+        trustableSites: [
+          { url: 'https://youtu.be/QHIw2hU3gqg' },
+          { url: 'https://youtu.be/5-X0wWjHGjI' }
+        ]
+      },
+      {
+        concept: 'Hibridização in situ por fluorescência (FISH)',
+        definition: 'Técnica citogenética usada para detectar e localizar a presença ou a ausência de determinadas sequências de DNA em cromossomos. Ela utiliza sondas fluorescentes que se ligam somente às partes do cromossomo a que eles apresentem um elevado grau de complementaridade de sequência.',
+        trustableSites: [
+          { url: 'http://www.scielo.br/pdf/aib/v79n4/a23v79n4.pdf' },
         ]
       }
     ]
   }, 
-  {
-    sectionTitle: 'I',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  }, 
-  {
-    sectionTitle: 'J',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'K',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'L',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
   {
     sectionTitle: 'M',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Malformação congênita',
+        definition: 'Pode ser definida como todo defeito na constituição de algum órgão ou conjunto de órgãos que determine uma anomalia morfológica estrutural presente no nascimento devido à causa genética ambiental ou mista.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://biomania.com.br/artigo/malformacoes-congenitas' },
+          { url: 'http://www.rmmg.org/artigo/detalhes/2329' }
         ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'N',
-    data: [
+      },
       {
-        concept: '',
-        definition: '',
+        concept: 'Mosaicismo',
+        definition: 'É uma falha genética que ocorre durante o desenvolvimento do embrião, que passa a ter diferentes linhagens celulares. O indivíduo mosaico tem dois materiais genéticos distintos: um formado pela união dos gametas do pai e da mãe, e o outro que aparece devido à mutação genética. Geralmente a mutação ocorre em uma célula do embrião, envolvendo a perda ou a duplicação de um cromossomo.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'O',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://youtu.be/Dz4N9HUdLGs' },
+          { url: 'https://www.trocandofraldas.com.br/mosaicismo/' }
         ]
       }
     ]
@@ -198,11 +176,11 @@ const cytogeneticsData = [
     sectionTitle: 'P',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Poliploidia',
+        definition: 'É a situação genética de núcleo celular, célula, ou organismo, em que há mais de dois conjuntos de cromossomas homólogos.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'http://www.scielo.br/scielo.php?script=sci_arttext&pid=S0103-84782001000100028' },
+          { url: 'http://periodicos.ufpel.edu.br/ojs2/index.php/CAST/article/view/904' }
         ]
       }
     ]
@@ -211,37 +189,11 @@ const cytogeneticsData = [
     sectionTitle: 'Q',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Quimerismo',
+        definition: 'É o nome que se dá a um animal que tem duas ou mais populações de células geneticamente distintas com origem em zigotos diferentes, ou seja, carrega mais de um tipo de DNA no organismo.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'R',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'S',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://youtu.be/Dz4N9HUdLGs' },
+          { url: 'https://youtu.be/4gL5cZhCL70' }
         ]
       }
     ]
@@ -250,93 +202,22 @@ const cytogeneticsData = [
     sectionTitle: 'T',
     data: [
       {
-        concept: '',
-        definition: '',
+        concept: 'Telômero',
+        definition: 'São estruturas constituídas por fileiras repetitivas de proteínas e DNA não codificante que formam as extremidades dos cromossomos. Sua principal função é impedir o desgaste do material genético e manter a estabilidade estrutural do cromossoma. Essa extremidade vai sendo perdida ao longo das divisões celulares e limita o número de mitoses que uma célula pode sofrer. Em outras palavras, esta região determina o envelhecimento celular.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'https://www.bbc.com/portuguese/geral-43601735' },
         ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'U',
-    data: [
+      },
       {
-        concept: '',
-        definition: '',
+        concept: 'Translocação',
+        definition: 'É uma anomalia cromossômica causada pelo rearranjo de partes entre cromossomos não-homólogos. Um gene de fusão pode ser criado quando a translocação se une a outros dois genes separadas, um evento que é comum no câncer.',
         trustableSites: [
-          { url: '' },
-          { url: '' }
+          { url: 'http://repositorio-racs.famerp.br/racs_ol/vol-14-4/ID249.pdf' },
+          { url: 'https://youtu.be/Y5JZ4QeVbao' }
         ]
       }
     ]
-  },
-  {
-    sectionTitle: 'V',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'W',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'X',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'Y',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
-  {
-    sectionTitle: 'Z',
-    data: [
-      {
-        concept: '',
-        definition: '',
-        trustableSites: [
-          { url: '' },
-          { url: '' }
-        ]
-      }
-    ]
-  },
+  }
 ];
 
 export { cytogeneticsData };
