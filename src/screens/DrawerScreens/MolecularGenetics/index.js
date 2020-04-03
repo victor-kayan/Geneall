1d24e1e4170f17648993a5e080f153560ce8afff
Victor Kayan <victor.vkal@gmail.com>
2020-04-03 03:30:03 -0300

    Create search filter functionality

--- src/screens/DrawerScreens/MolecularGenetics/index.js
+++ src/screens/DrawerScreens/MolecularGenetics/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Text, TextInput } from 'react-native';
 
 import AlphabeticalList from '../../../components/AlphabeticalList';
 import Header from '../../../components/Header';
@@ -6,10 +7,45 @@ import { molecularGeneticsData } from '../../../../assets/glossaryData/Molecular
 import { Container } from './styles';
 
 export default function MolecularGenetics() {
+  const [ filteredData, setFilteredData ] = useState(molecularGeneticsData );
+
+  function filterGlossaryData(term) {
+    const searchText = term.trim().toLowerCase();
+
+    let filteredGlossary = [];
+
+    for (section of molecularGeneticsData) {
+      const filteredDataInSection = section.data.filter(item => {
+        return item.concept.trim().toLowerCase().match(searchText);
+      });
+
+      if (filteredDataInSection.length > 0) {
+        const filteredSection = { 
+          sectionTitle: section.sectionTitle, 
+          data: filteredDataInSection 
+        };
+        filteredGlossary.push(filteredSection);
+      }
+    }
+
+    setFilteredData(filteredGlossary);
+  }
+
   return (
     <Container>
       <Header title='Genética Molecular' />
-      <AlphabeticalList data={molecularGeneticsData} />
+      <TextInput
+        onChangeText={(term) => { filterGlossaryData(term) }}
+        placeholder="Type a message to search"
+      />
+
+      { filteredData.length > 0
+      ? 
+        <AlphabeticalList data={ filteredData } />
+      : 
+        <Text>Oops, nenhum item corresponde a sua pesquisa :/</Text>
+      }
     </Container>
   );
 }
+
