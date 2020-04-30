cefa911b4347d20cdad991118063e7048fa0036f
Victor Kayan <victor.vkal@gmail.com>
2020-04-29 22:10:28 -0300

    Refactor SideBar component Use functional component and styled-components.

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,163 +1,96 @@
-import React, { Component } from "react";
-import { ImageBackground, Platform, Dimensions, StyleSheet } from "react-native";
+import React from "react";
+import { View } from 'react-native';
 
-import {
-  Content,
-  Text,
-  CardItem,
-  Container,
-  Left,
-  View,
-} from "native-base";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  CardItem,
+  Left,
+} from "native-base";
 
 import Images from '../../../assets/images';
 import Colors from '../../../assets/colors'
+import {
+  Content,
+  DrawerCover,
+  DarkImageCover,
+  Title,
+  Description,
+  RouteName,
+  Separator
+} from './styles';
 
-const routesData = [
-  {
-    id: 0,
-    name: "Início",
-    route: "Home",
-    icon: "home"
-  }, {
-    id: 1,
-    name: "Genética Molecular",
-    route: "MolecularGenetics",
-    icon: "molecule"
-  }, {
-    id: 2,
-    name: "Genética da Transmissão",
-    route: "TransmissionGenetic",
-    icon: "transition"
-  }, {
-    id: 3,
-    name: "Citogenética",
-    route: "Cytogenetic",
-    icon: "microscope"
-  }, {
-    id: 4,
-    name: "Indicações de Livros",
-    route: "Books",
-    icon: "library-books"
-  }, {
-    id: 5,
-    name: "Sobre o Aplicativo",
-    route: "AboutThisApp",
-    icon: "information-outline"
-  },
-];
+export default function SideBar() {
+  const navigation = useNavigation();
 
-export default class SideBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      shadowOffsetWidth: 1,
-      shadowRadius: 4
-    };
-  }
+  const routesData = [
+    {
+      id: 0,
+      name: "Início",
+      route: "Home",
+      icon: "home"
+    }, {
+      id: 1,
+      name: "Genética Molecular",
+      route: "MolecularGenetics",
+      icon: "molecule"
+    }, {
+      id: 2,
+      name: "Genética da Transmissão",
+      route: "TransmissionGenetic",
+      icon: "transition"
+    }, {
+      id: 3,
+      name: "Citogenética",
+      route: "Cytogenetic",
+      icon: "microscope"
+    }, {
+      id: 4,
+      name: "Indicações de Livros",
+      route: "Books",
+      icon: "library-books"
+    }, {
+      id: 5,
+      name: "Sobre o Aplicativo",
+      route: "AboutThisApp",
+      icon: "information-outline"
+    },
+  ];
 
-  render() {
-    return (
-      <Container>
-        <Content
-          bounces={false}
-          style={{ flex: 1, backgroundColor: "#fff", top: -1 }}
-        >
-          <ImageBackground source={Images.sideBar.drawerCover} style={styles.drawerCover}> 
-            <View style={styles.darkImageCover}> 
-              <Text style={styles.title}>Geneall</Text>
-              <Text style={styles.description}>Aprenda genética</Text>
-            </View>
-          </ImageBackground>
+  return (
+    <Container>
+      <Content bounces={false}>
+        <DrawerCover source={Images.sideBar.drawerCover}> 
+          <DarkImageCover> 
+            <Title>Geneall</Title>
+            <Description>Aprenda genética</Description>
+          </DarkImageCover>
+        </DrawerCover>
 
-          <View>
-            { routesData && Object.keys(routesData).length
-              ? routesData.map(data => (
-                <View key={data.id} >
-                  <CardItem
-                    button
-                    noBorder
-                    onPress={() => this.props.navigation.navigate(data.route)}
-                  >
-                    <Left>
-                      <Icon name={data.icon} size={30} color={Colors.PRIMARY_BLUE} />
-                      <Text style={styles.text}>{data.name}</Text>
-                    </Left>
-                  </CardItem>
-                  
-                  { data.route === 'Home' || data.route === 'Cytogenetic' ? 
-                    <View style={styles.separator}/>
-                  : null }
-                </View>
-                ))
-              : null }
-          </View>
-        </Content>
-      </Container>
-    );
-  }
+        <View>
+          { routesData && Object.keys(routesData).length
+            ? routesData.map(data => (
+              <View key={data.id} >
+                <CardItem
+                  button
+                  noBorder
+                  onPress={() => navigation.navigate(data.route)}
+                >
+                  <Left>
+                    <Icon name={data.icon} size={30} color={Colors.PRIMARY_BLUE} />
+                    <RouteName>{data.name}</RouteName>
+                  </Left>
+                </CardItem>
+                
+                { data.route === 'Home' || data.route === 'Cytogenetic' ? 
+                  <Separator />
+                : null }
+              </View>
+              ))
+            : null }
+        </View>
+      </Content>
+    </Container>
+  );
 }
-
-const deviceHeight = Dimensions.get("window").height;
-const deviceWidth = Dimensions.get("window").width;
-
-const styles = StyleSheet.create({
-  drawerCover: {
-    alignSelf: "stretch",
-    height: deviceHeight / 3.5,
-    width: null,
-    position: "relative",
-    marginBottom: 10,
-  },
-  darkImageCover: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems:'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.3)'
-  },
-  title: {
-    color: '#FFF',
-    fontSize: 50,
-    marginBottom: 5,
-    fontFamily: 'Mollen Personal Use-Bold',
-  },
-  description: {
-    fontSize: 16,
-    marginHorizontal: 20,
-    color: '#FFF',
-    textAlign: 'center',
-    fontFamily: 'Mollen Personal Use-Bold',
-  },
-  drawerImage: {
-    position: "absolute",
-    left: Platform.OS === "android" ? deviceWidth / 10 : deviceWidth / 9,
-    top: Platform.OS === "android" ? deviceHeight / 13 : deviceHeight / 12,
-    width: 210,
-    height: 75,
-    resizeMode: "cover"
-  },
-  text: {
-    fontSize: 16,
-    marginLeft: 20,
-    color: '#333'
-  },
-  textDivider: {
-    fontWeight: Platform.OS === "ios" ? "500" : "400",
-    fontSize: 12,
-    marginLeft: 20
-  },
-  badgeText: {
-    fontSize: Platform.OS === "ios" ? 13 : 11,
-    fontWeight: "400",
-    textAlign: "center",
-    marginTop: Platform.OS === "android" ? -3 : undefined
-  },
-  separator: {
-    backgroundColor: "#CCC",
-    height: 1,
-    width: '76%',
-    marginVertical: 10,
-    alignSelf: "flex-end"
-  }
-});
---
+++ src/components/SideBar/styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components/native';
+import { Content as NativeBaseContent } from 'native-base';
+
+import Colors from '../../../assets/colors';
+import { SCREEN_HEIGHT } from '../../../assets/constants';
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Content = styled(NativeBaseContent)`
+  flex: 1; 
+  background-color: #FFF;
+`;
+
+export const DrawerCover = styled.ImageBackground`
+  align-self: stretch;
+  height: ${SCREEN_HEIGHT / 3.5}px;
+  width: null;
+  position: relative;
+  margin-bottom: 10px;
+`;
+
+export const DarkImageCover = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items:center;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+
+export const Title = styled.Text`
+  color: #FFF;
+  font-size: 50px;
+  margin-bottom: 5px;
+  font-family: 'Mollen Personal Use-Bold';
+`;
+
+export const Description = styled.Text`
+  font-size: 16px;
+  margin: 0 20px;
+  color: #FFF;
+  text-align: center;
+  font-family: 'Mollen Personal Use-Bold';
+`;
+
+export const RouteName = styled.Text`
+  font-size: 16px;
+  margin-left: 20px;
+  color: ${Colors.BLACK};
+`;
+
+export const Separator = styled.View`
+  background-color: #CCC;
+  height: 1;
+  width: 76%;
+  margin: 10px 0;
+  align-self: flex-end;
+`;
